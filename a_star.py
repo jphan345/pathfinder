0e0c4d66b95fb82aa06a901aabf58b791adc6e25
jphan345 <71359269+jphan345@users.noreply.github.com>
2020-09-30 16:17:34 -0700

    Update a_star.py

--- a_star.py
+++ a_star.py
@@ -1,19 +1,26 @@
-START = 'A'
-END = 'B'
-OBSTACLE = '*'
+from pathfinding import *
 
 
-class Node:
-    def __init__(self, coordinates: tuple, movement_cost: int, f: int, parent=None):
-        self.x = coordinates[0]
-        self.y = coordinates[1]
-        self.movement_cost = movement_cost
-        self.f = f
-        self.parent = parent  # Node
+def manhattan_heuristic(coordinates: tuple, end: tuple) -> int:
+    """Estimated movement cost from given square to the destination."""
+    return abs(coordinates[0] - end[0]) + abs(coordinates[1] - end[1])
 
 
-def a_star(board: list):
-    """A* pathfinding algorithm."""
+def diagonal_heuristic(coordinates: tuple, end: tuple) -> int:
+    return max(abs(coordinates[0] - end[0]), abs(coordinates[1] - end[1]))
+
+
+def find_next_node(nodes: list) -> int:
+    """Return the index of the node with the lowest f (total movement cost)."""
+    index = 0
+    for i in range(len(nodes)):
+        if nodes[i].f < nodes[index].f:
+            index = i
+    return index
+
+
+def search(board: list) -> Node:
+    """Text-based version of A* pathfinding algorithm."""
     open_list = []
     closed_list = []
 
@@ -33,11 +40,10 @@ def a_star(board: list):
             s_f = s_movement_cost + manhattan_heuristic(coordinates, end_coordinates)
 
             skip = False
-            for node in open_list:
-                if coordinates == (node.x, node.y) and node.f < s_f:
-                    skip = True
-            for node in closed_list:
-                if coordinates == (node.x, node.y) and node.f < s_f:
+            combined_list = open_list[:]
+            combined_list.extend(closed_list[:])
+            for node in combined_list:
+                if coordinates == (node.x, node.y) and node.f <= s_f:
                     skip = True
 
             if not skip:
@@ -46,65 +52,50 @@ def a_star(board: list):
         closed_list.append(parent)
 
 
-def manhattan_heuristic(coordinates: tuple, end: tuple) -> int:
-    """Estimated movement cost from given square to the destination."""
-    return abs(coordinates[0] - end[0]) + abs(coordinates[1] - end[1])
+def visual_search(self):
+    """Visual a_star algorithm."""
+    import pygame
+    import sys
 
+    open_list = []
+    closed_list = []
 
-def find_node(board: list, node_name: str) -> tuple:
-    """Find a node in the board and return the coordinates."""
-    for row in range(len(board)):
-        for col in range(len(board[row])):
-            if board[row][col] == node_name:
-                return row, col
+    # (coordinates: tuple, current_movement_cost: int, f: int)
+    open_list.append(Node(self.start_coordinates, 0, 0))
+    while open_list:
+        parent = open_list.pop(find_next_node(open_list))
+        for coordinates in generate_successors(self.board, (parent.x, parent.y)):
+            if self.board[coordinates[0]][coordinates[1]] == self.END:
+                return Node(coordinates, parent.movement_cost + 1, parent.f, parent)
 
+            if self.board[coordinates[0]][coordinates[1]] == self.EMPTY:
+                self.board[coordinates[0]][coordinates[1]] = self.SUCCESSOR
 
-def find_next_node(nodes: list) -> int:
-    """Return the index of the node with the lowest f (total movement cost)."""
-    index = 0
-    for i in range(len(nodes)):
-        if nodes[i].f < nodes[index].f:
-            index = i
-    return index
+            self.display.fill(self.DARK_BLUE)
+            self.draw_grid()
+            self.draw_buttons()
+            pygame.display.update()
 
+            s_movement_cost = parent.movement_cost + 1
+            s_f = s_movement_cost + manhattan_heuristic(coordinates, self.end_coordinates)
 
-def generate_successors(board, coordinates) -> tuple:
-    """Generate the given node's successors.
-    Yield the horizontal and vertical successors before the diagonal successors."""
+            skip = False
+            combined_list = open_list[:]
+            combined_list.extend(closed_list)
+            for node in combined_list:
+                if coordinates == (node.x, node.y) and node.f <= s_f:
+                    skip = True
+            if not skip:
+                open_list.append(Node(coordinates, s_movement_cost, s_f, parent))
 
-    def check(coord_x: int, coord_y: int) -> bool:
-        # Checks to see if a coordinate is valid to move to
-        return 0 <= coord_x <= len(board) - 1 and 0 <= coord_y <= len(board[coord_x]) - 1 and \
-               (coord_x, coord_y) != coordinates and board[coord_x][coord_y] != OBSTACLE
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if 780 < mouse_pos[0] < 920 and 15 < mouse_pos[1] < 60:
+                    open_list = []
+                    self.reset()
 
-    def check_diagonal(coord_1: tuple, coord_2: tuple) -> bool:
-        # Checks a diagonal to see if the path can go there.
-        # Used to prevent cases where the path goes through a wall that is connected together by its corners.
-        x_diff = coord_2[0] - coord_1[0]
-        y_diff = coord_2[1] - coord_1[1]
-        return board[coord_1[0] + x_diff][coord_1[1]] != OBSTACLE or board[coord_1[0]][coord_1[1] + y_diff] != OBSTACLE
-
-    # Yield the horizontal and vertical successors
-    for i in [1, -1]:
-        x, y = coordinates[0] + i, coordinates[1] + i
-        if check(x, coordinates[1]):
-            yield x, coordinates[1]
-        if check(coordinates[0], y):
-            yield coordinates[0], y
-
-    # Yield the diagonal successors
-    for i in [1, -1]:
-        for j in [1, -1]:
-            x, y = coordinates[0] + i, coordinates[1] + j
-            if check(x, y) and check_diagonal(coordinates, (x, y)):
-                yield x, y
-
-
-def print_board(board: list) -> None:
-    for i in board:
-        for j in i:
-            if j == START or j == END:
-                print(f'*{j}*', end='')
-            else:
-                print(f' {j} ', end='')
-        print()
+        closed_list.append(parent)

a2305dbb823ec3f035c89139b8832c1f439eb163
jphan345 <71359269+jphan345@users.noreply.github.com>
2020-10-03 17:34:51 -0700

    Update a_star.py

--- a_star.py
+++ a_star.py
@@ -1,4 +1,4 @@
-from pathfinding import *
+from algorithms.pathfinding import *
 
 
 def manhattan_heuristic(coordinates: tuple, end: tuple) -> int:
@@ -13,7 +13,7 @@ def diagonal_heuristic(coordinates: tuple, end: tuple) -> int:
 def find_next_node(nodes: list) -> int:
     """Return the index of the node with the lowest f (total movement cost)."""
     index = 0
-    for i in range(len(nodes)):
+    for i in range(len(nodes) - 1, -1, -1):
         if nodes[i].f < nodes[index].f:
             index = i
     return index
@@ -37,6 +37,8 @@ def search(board: list) -> Node:
                 return Node(coordinates, parent.movement_cost + 1, parent.f, parent)
 
             s_movement_cost = parent.movement_cost + 1
+            if board[coordinates[0]][coordinates[1]] == WEIGHT:
+                s_movement_cost += 5
             s_f = s_movement_cost + manhattan_heuristic(coordinates, end_coordinates)
 
             skip = False
@@ -53,7 +55,10 @@ def search(board: list) -> Node:
 
 
 def visual_search(self):
-    """Visual a_star algorithm."""
+    """Visual a_star algorithm.
+    variables:: self.display, self.board, self.start_coordinates, self.end_coordinates, self.EMPTY, self.SUCCESSOR,
+                self.DARK_BLUE
+    functions:: self.draw_grid, self.draw_buttons, self.reset"""
     import pygame
     import sys
 
@@ -64,12 +69,14 @@ def visual_search(self):
     open_list.append(Node(self.start_coordinates, 0, 0))
     while open_list:
         parent = open_list.pop(find_next_node(open_list))
-        for coordinates in generate_successors(self.board, (parent.x, parent.y)):
-            if self.board[coordinates[0]][coordinates[1]] == self.END:
-                return Node(coordinates, parent.movement_cost + 1, parent.f, parent)
+        if self.board[parent.x][parent.y] == self.EMPTY:
+            self.board[parent.x][parent.y] = self.SUCCESSOR
+        if self.board[parent.x][parent.y] == self.WEIGHT:
+            self.board[parent.x][parent.y] = self.WEIGHTED_SUCCESSOR
 
-            if self.board[coordinates[0]][coordinates[1]] == self.EMPTY:
-                self.board[coordinates[0]][coordinates[1]] = self.SUCCESSOR
+        for coordinates in generate_successors(self.board, (parent.x, parent.y)):
+            if coordinates == self.end_coordinates:
+                return Node(coordinates, parent.movement_cost + 1, parent.f, parent)
 
             self.display.fill(self.DARK_BLUE)
             self.draw_grid()
@@ -77,6 +84,8 @@ def visual_search(self):
             pygame.display.update()
 
             s_movement_cost = parent.movement_cost + 1
+            if self.board[coordinates[0]][coordinates[1]] == self.WEIGHT:
+                s_movement_cost += 5
             s_f = s_movement_cost + manhattan_heuristic(coordinates, self.end_coordinates)
 
             skip = False
